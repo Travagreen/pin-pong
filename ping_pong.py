79ab42f3f78d3242ed334e7169023d51fec2faf7
Travagreen <147731104+Travagreen@users.noreply.github.com>
2024-03-02 19:47:46 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -3,12 +3,28 @@ window = display.set_mode((700,500))
 
 display.set_caption('ПинПонг')
 clock = time.Clock()
-backgroun = transform.scale(image.load(),(700,500)
+backgroun = transform.scale(image.load('background.png'),(700,500))
 
-while Game():
+class GameSprite(sprite.Sprite):
+    def __init__(self,filename,x,y,width,height,speed):
+        super().__init__()
+        self.image = transform.scale(image.load(filename),(width,height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+class Player(GameSprite):
+    pass
+ball = GameSprite('шарык.png',20,30,40,20,10)
+clock = time.Clock()
+game = True
+finish = True
+while game:
     for e in event.get():
         if e.type == QUIT:
-            game == False:
+            game == False
 
-    display.uptade()
+    display.update()
     clock.tick(60)
